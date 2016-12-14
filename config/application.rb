c8879992158b3fdf66da5cdb3694643d59e4781a
congago <tnngocthinh4@gmail.com>
2016-12-14 13:12:14 +0700

    init project

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+#config.i18n.default_locale = :vn
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module App
+  class Application < Rails::Application
+  	config.i18n.default_locale = :en
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
